2836edb8e863b6e34e771112597deed1cbe694e9
David Tekeste <tekestede@vcu.edu>
2024-04-08 23:16:38 -0400

    Finished

---
+++ README.md
@@ -0,0 +1,57 @@
+# Project 7 - *Tasks*
+
+Submitted by: **Dawit Tekeste**
+
+**Tasks** is an app that saves users tasks by also adding a calendar feature to give deadlines to tasks
+
+Time spent: **3** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [X] App displays a list of tasks
+- [X] Users can add tasks to the list
+- [X] Session persists when application is closed and relaunched (tasks dont get deleted when closing app) 
+  - [X] Note: You have to quit the app, not minimize it, in order to see the persistence.
+- [X] Tasks can be deleted
+- [X] Users have a calendar view via navigation controller that displays tasks	
+
+
+The following **additional** features are implemented:
+
+- [X] Tasks can be toggled completed
+- [X] User can edit tasks by tapping on the task in the feed view
+- [ ] List anything else that you can get done to improve the app functionality!
+
+## Video Walkthrough
+
+Here is a reminder on how to embed Loom videos on GitHub. Feel free to remove this reminder once you upload your README. 
+
+<div>
+    <a href="https://www.loom.com/share/de383d0a762146cc9318523c8d73129d">
+      <img style="max-width:300px;" src="https://cdn.loom.com/sessions/thumbnails/de383d0a762146cc9318523c8d73129d-with-play.gif">
+    </a>
+  </div>
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+N/A
+
+## License
+
+    Copyright [2024] [Dawit Tekeste]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- ios101-project7-task/Task.swift
+++ ios101-project7-task/Task.swift
@@ -5,7 +5,7 @@
 import UIKit
 
 // The Task model
-struct Task {
+struct Task: Codable {
 
     // The task's title
     var title: String
@@ -44,10 +44,10 @@ struct Task {
 
     // The date the task was created
     // This property is set as the current date whenever the task is initially created.
-    let createdDate: Date = Date()
+    var createdDate: Date = Date()
 
     // An id (Universal Unique Identifier) used to identify a task.
-    let id: String = UUID().uuidString
+    var id: String = UUID().uuidString
 }
 
 // MARK: - Task + UserDefaults
@@ -56,21 +56,50 @@ extension Task {
 
     // Given an array of tasks, encodes them to data and saves to UserDefaults.
     static func save(_ tasks: [Task]) {
-
+        
         // TODO: Save the array of tasks
+        let defaults = UserDefaults.standard
+        let encodedData = try! JSONEncoder().encode(tasks)
+        defaults.set(encodedData, forKey: "tasksKey")
     }
 
     // Retrieve an array of saved tasks from UserDefaults.
     static func getTasks() -> [Task] {
         
         // TODO: Get the array of saved tasks from UserDefaults
-
-        return [] // 👈 replace with returned saved tasks
+        let defaults = UserDefaults.standard
+        if let data = defaults.data(forKey: "tasksKey"){
+            let decodedContacts = try! JSONDecoder().decode([Task].self, from: data)
+            return decodedContacts
+        } else {
+            return []
+        }
     }
 
     // Add a new task or update an existing task with the current task.
     func save() {
-
+        
         // TODO: Save the current task
+        var saved_tasks = Task.getTasks()
+        
+        var i = 0
+        var inserted = false
+        while (i < saved_tasks.count) {
+            if self.id == saved_tasks[i].id {
+                saved_tasks.remove(at: i)
+                saved_tasks.insert(self, at: i)
+                inserted = true
+                break
+            }
+            
+            i += 1
+        }
+        
+        if inserted == false {
+            saved_tasks.append(self)
+        }
+        
+        Task.save(saved_tasks)
+
     }
 }
